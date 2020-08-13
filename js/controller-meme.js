36255618e08cac6f8645e70e7a36e4af2b763d21
lior_ <liorgobari2@gmail.com>
2020-08-13 21:04:42 +0300

    update2

--- js/controller-meme.js
+++ js/controller-meme.js
@@ -6,24 +6,20 @@ var gCtx;
 function onInit() {
     gCanvas = document.getElementById('myCanvas');
     gCtx = gCanvas.getContext('2d');
-    drawImg()
+    // drawImg()
     clearCanvas()
-    // renderImgs()
+    renderGalleryImgs()
 
 }
 // To do:
 // . Create a Canvas with a single image – the image shall be taken from
 // gMeme (managed by a memeService)
 
-// function drawImg() {
-//     const elImg = document.querySelector('img');
-//     gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
-// }
-
 function drawImg() {
-    // let currImg = gImgs;
+    // var currImg = getImgById(gImgs.id)
+    // var currImg = getImgs()
     const img = new Image();
-    img.src = gImgs[0].url;
+    img.src = getImgUrl();
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
         drawText(gMeme.line.txt, 225, 225)
@@ -38,7 +34,8 @@ function drawText(text, x, y) {
     gCtx.lineWidth = '2';
     gCtx.strokeStyle = 'black';
     gCtx.fillStyle = 'white';
-    gCtx.font = '40px Impact';
+    gCtx.font = `${gMeme.line.size}px Impact`
+    // '40px Impact';
     gCtx.textAlign = 'center';
     gCtx.fillText(text, x, y);
     gCtx.strokeText(text, x, y);
@@ -57,21 +54,46 @@ function onAddMemeTxt(text) {
 // gMeme and present it onto the Canvas. Note that to start with – locate
 // the Editor above the Image-Gallery.
 
-// function renderImgs() {
-//     var imgs = getImgs();
-//     console.log(imgs);
-//     var strHTMLs = imgs.map(function (img) {
-//         return `<img src="${img.imgUrl}" onclick="drawImg()">`
-//     });
-//     document.querySelector('.gallery').innerHTML = strHTMLs.join('');
-// }
+function renderGalleryImgs() {
+    var imgs = getImgs();
+    console.log(imgs);
+    var strHTMLs = imgs.map(function (img) {
+        return `<img onclick="onSelectImg(${img.id})" src="${img.url}" ${img.keywords}>`
+    });
+    document.querySelector('.gallery').innerHTML = strHTMLs.join('');
+}
 
 
-// i. Make sure you can access your project in gitPages
+function onSelectImg(id) {
+    // gMeme.selectedImgId = gImgs.id;
+    var img = getImgById(id);
+    drawImg(img)
+    document.querySelector('.modal-editor').style.display = 'block';
+    document.querySelector('.gallery').style.display = 'none';
+    document.querySelector('.keyword-nav').style.display = 'none';
+}
+
+function onIncreaseFont() {
+    increaseFontSize()
+    drawImg()
+    // drawText(gMeme.line.txt, 225, 225)
+}
+
+function onDecreaseFont() {
+    decreaseFontSize()
+    drawImg()
+}
 
 
+// to-do
+// function onUpLine(){}
+
+// function onDownLine(){}
+
+// function onSwitchLines(){}
 
 
 function clearCanvas() {
     gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
 }
+
--- js/service-meme.js
+++ js/service-meme.js
@@ -1,9 +1,19 @@
 'use strict';
 // var gKeywords = { 'happy': 12, 'funny puk': 1 }
-var gNextId = 1;
-var gImgs= [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] }, { id: 2, url: 'img/2.jpg', keywords: ['sad'] }];
+// var gNextId = 1;
+
+var gImgs = [
+    { id: 0, url: 'img/1.jpg', keywords: ['happy'] },
+    { id: 1, url: 'img/2.jpg', keywords: ['dog'] },
+    { id: 2, url: 'img/3.jpg', keywords: ['dog'] },
+    { id: 3, url: 'img/4.jpg', keywords: ['cat'] },
+    { id: 4, url: 'img/5.jpg', keywords: ['win'] },
+    { id: 5, url: 'img/6.jpg', keywords: ['tired'] },
+    { id: 6, url: 'img/7.jpg', keywords: ['what???!'] },
+    { id: 7, url: 'img/8.jpg', keywords: ['happy'] },
+];
 var gMeme = {
-    selectedImgId: 1,
+    selectedImgId: 0,
     selectedLineIdx: 0,
     line: {
         txt: 'I never eat Falafel',
@@ -14,7 +24,6 @@ var gMeme = {
     }
 }
 
-// createImgsGallery();
 
 function addMemeTxt(text) {
     gMeme.line.txt = text;
@@ -23,25 +32,37 @@ function addMemeTxt(text) {
 }
 
 
-// function createImgsGallery() {
-//     gImgs = [];
-//     gImgs.push(createImgGallery('img/1.jpg'));
-//     gImgs.push(createImgGallery('img/2.jpg'));
-//     // gImgs.push(createImgGallery());
-//     // gImgs.push(createImgGallery());
-
-// }
-
-function createImgGallery(imgUrl) {
-    return {
-        id: gNextId,
-        imgUrl:imgUrl
-    }
-
+function getImgById(selectedImgId) {
+    var selectedImg = gImgs.find(function (selectedImg) {
+        return selectedImgId === selectedImg.id
+    })
+    console.log(selectedImg);
+    // selectedImgId++
+    return selectedImg
 }
+
+function creatMeme(selectedImgId) {
+    gMeme = {
+        selectedImgId: selectedImgId
+    }
+}
+
+
+function getImgUrl() {
+    return getImgById(gMeme.selectedImgId).url
+}
+
+
 function getImgs() {
     return gImgs;
 }
 
 
+function increaseFontSize() {
+    gMeme.line.txt.size++;
+}
+
+function decreaseFontSize() {
+    gMeme.line.txt.size--;
+}
 
